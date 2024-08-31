42d95c992cd93fcc7bf11e099fe169c0f1b483c2
RobertoFausti <roberto.fausti@stiima.cnr.it>
2024-08-31 16:24:45 +0200

    launch file ignore ros__parameters + removed needless yaml splitting

--- ik_solver/launch/ik_solver.launch.py
+++ ik_solver/launch/ik_solver.launch.py
@@ -22,8 +22,6 @@ def launch_setup(context, *args, **kwargs):
 
   if os.path.exists(config_path):
     param_dir = os.environ["CNR_PARAM_ROOT_DIRECTORY"]
-    prefix_path = Path(param_dir) / 'ik_solver_param'
-    load_plugin_param_proc = list()
     launch_node_after_load = list()
     ret = []
 
@@ -37,34 +35,27 @@ def launch_setup(context, *args, **kwargs):
 
       else:
         # Each first level parameter correspond to a new node to launch
+
+        cnr_param_server_process = ExecuteProcess(
+          cmd = [[
+            FindExecutable(name="cnr_param_server"),
+            " --path-to-file ",
+            config_path
+          ]],
+          shell=True,
+        )
+
         for yaml_node in yaml_struct.keys():
 
-          # Check directory
-          if not os.path.isdir(prefix_path):
-            os.makedirs(prefix_path)
-
-          # Temporary parameter file to save the portion of yaml that correspond to a single node
-          segment_file = prefix_path / yaml_node
-          with open(segment_file, 'w') as tmp:
-            yaml.dump({yaml_node : yaml_struct[yaml_node]}, tmp)
-
-          # Load parameters of the node into cnr parameter server
-          load_plugin_param_proc.append(
-            ExecuteProcess(
-              cmd = [[
-                FindExecutable(name="cnr_param_server"),
-                " --path-to-file ",
-                segment_file.as_posix()
-              ]],
-              shell=True,
-            )
-          )
+          # Ignore ROS parameters
+          if 'ros__parameters' in yaml_struct[yaml_node].keys():
+            continue
 
           # Launch ik_solver_node after parameters has been properly loaded
           # Each ik_solver_node has a namespace equal to the 1st-level parameter
           launch_node_after_load.append(RegisterEventHandler(
             OnProcessExit(
-              target_action=load_plugin_param_proc[-1],
+              target_action=cnr_param_server_process,
               on_exit=Node(
                             package="ik_solver",
                             executable="ik_solver_node",
@@ -77,7 +68,7 @@ def launch_setup(context, *args, **kwargs):
             )
           ))
 
-    ret =  [*load_plugin_param_proc,
+    ret =  [cnr_param_server_process,
             *launch_node_after_load]
 
   return ret
